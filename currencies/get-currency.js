0c7ef2d0e31af53e09323c64929496691c47a278
Mateusz Suchoń <kontakt@suchego.pl>
2021-11-29 13:05:45 +0100

    Fix currency api field.

--- currencies/get-currency.js
+++ currencies/get-currency.js
@@ -3,8 +3,8 @@ const axios = require('axios');
 module.exports = async (currency) => {
   const url = `https://systemkantor.aliorbank.pl/forex/json/current/${currency}/PLN`;
   const { data } = await axios.get(url);
-  const { sell } = data.currencies[0];
-  const amountString = sell.replace(',', '.');
+  const { buy } = data.currencies[0];
+  const amountString = buy.replace(',', '.');
   const amount = parseFloat(amountString).toPrecision(3)
 
   return amount;
